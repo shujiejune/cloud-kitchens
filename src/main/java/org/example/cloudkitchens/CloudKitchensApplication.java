0c4cc6b90094aad8f1a25217adbeda5f08f23c81
shujiejune <jieniu.june@tutanota.com>
2026-04-15 23:39:51 -0700

    build: parent pom.xml

---
+++ src/main/java/org/example/cloudkitchens/CloudKitchensApplication.java
@@ -0,0 +1,13 @@
+package org.example.cloudkitchens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudKitchensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudKitchensApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cloudkitchens/CloudKitchensApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cloudkitchens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudKitchensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
