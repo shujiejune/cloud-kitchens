803b9918a724f83329cba3ccd74d3aa270ce56e9
shujiejune <jieniu.june@tutanota.com>
2026-04-17 22:12:11 -0700

    feat: microservices skeleton

---
+++ procurement-service/src/main/java/org/example/procurementservice/ProcurementServiceApplication.java
@@ -0,0 +1,11 @@
+package org.example.procurementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProcurementServiceApplication.class, args);
+    }
+}
